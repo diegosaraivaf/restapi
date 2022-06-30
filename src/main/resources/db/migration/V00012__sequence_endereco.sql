e0d380fce29fcbb60ceab18f3eae63469f1dc698
DIEGO-SARAIVA\diego.ferreira <diego.ferreira@aspec.com.br>
2022-06-30 10:35:06 -0300

    correcao do script de criacao de sequence

--- src/main/resources/db/migration/V00012__sequence_endereco.sql
+++ src/main/resources/db/migration/V00012__sequence_endereco.sql
@@ -1,4 +1,4 @@
 begin;
-CREATE SEQUENCE endereco_id_seq AS bigint START 1 OWNED BY endereco.id;
+CREATE SEQUENCE endereco_id_seq START 1 ;
 ALTER TABLE endereco ALTER COLUMN id SET DEFAULT nextval('endereco_id_seq');
 commit;
