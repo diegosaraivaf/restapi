911ea68da53ed6f19249d1f9b1d2344432556cd8
DIEGO-SARAIVA\diego.ferreira <diego.ferreira@aspec.com.br>
2022-07-04 15:50:50 -0300

    adicionada axios.defaults.baseURL, adicionado consulta de contribuinte, adicionado exclusao de contribuinte

--- src/main/resources/public/cadastroContribuinte.js
+++ src/main/resources/public/cadastroContribuinte.js
@@ -4,34 +4,44 @@
 //	manipularFormulario()
 //}
 
+axios.defaults.baseURL = 'http://localhost:8080'
+
 const post = () => {
 	const form_lancamento = document.getElementById('form')
 	
 //	form_lancamento.onsubmit = async (event) => {
-		event.preventDefault()
+//		event.preventDefault()
 
-		const inputNome = document.getElementById('nome')
-		const inputDocumento = document.getElementById('documento')
-		const inputRua1 = document.getElementById('rua1')
-		const inputBairro1 = document.getElementById('bairro1')
-		const inputRua2 = document.getElementById('rua2')
-		const inputBairro3 = document.getElementById('bairro2')
+	const inputNome = document.getElementById('nome')
+	const inputDocumento = document.getElementById('documento')
+	const inputRua1 = document.getElementById('rua1')
+	const inputBairro1 = document.getElementById('bairro1')
+	const inputRua2 = document.getElementById('rua2')
+	const inputBairro3 = document.getElementById('bairro2')
 		
-		//pegar o id do contribuinte para passar como parametro na insercao de endereco
-		console.log("salvando contribuinte")
-		const data  = {
-			nome : inputNome.value,
-			documento : inputDocumento.value
-		}
+	//pegar o id do contribuinte para passar como parametro na insercao de endereco
+	const data  = {
+		nome : inputNome.value,
+		documento : inputDocumento.value
+	}
 //		await axios.post('http://localhost:8080/contribuintes', data)
-		axios.post('http://localhost:8080/contribuintes', data)
-		
-		console.log("salvando endereco")
-		axios.post('http://localhost:8080/enderecos',{
-			rua : inputRua1.value,
-			bairro : inputBairro1.value
+	axios.post('contribuintes', data)
+		.then(response => {
+			console.log('Contribuinte cadastrado')
+		}).catch(error =>{ 
+			console.log('Erro ao cadastrar contribuinte')
 		})
-		
+	
+	axios.post('enderecos',{
+		rua : inputRua1.value,
+		bairro : inputBairro1.value
+	})
+		.then(response => {
+			console.log('Endereco cadastrado')
+		}).catch(error =>{ 
+			console.log('Erro ao cadastrar endereco')
+		})
+	
 		//window.location.href = "http://localhost:8080/consultaLancamento.html";
 //	}
 }
@@ -41,7 +51,7 @@ const put = () => {
 		nome : 'teste',
 		documento :'teste',
 	}
-	axios.put('http://localhost:8080/contribuintes/1',contribuinte)
+	axios.put('contribuintes/1',contribuinte)
 		.then(response => {
 			console.log('consitruinte atualizado com sucesso')
 		}).catch(error => {
@@ -54,7 +64,7 @@ const put = () => {
 		numero : 'teste',
 		cep : 'teste'
 	}
-	axios.put('http://localhost:8080/enderecos/1',endereco)
+	axios.put('enderecos/1',endereco)
 		.then(response => {
 			console.log('endereco atualizado')
 		}).catch(error => {
@@ -69,9 +79,11 @@ const get = () => {
 		}
 	};
 	
-	axios.get('http://localhost:8080/contribuintes', config)
+	axios.get('contribuintes', config)
 		.then(response => { 
 			console.log(response.data)
+			
+			loadTableData(response.data)
 		})
 		.catch(error => {
 		    console.log(error);
@@ -79,8 +91,8 @@ const get = () => {
 	
 }
 
-const delet = () => {
-	axios.delete('http://localhost:8080/contribuintes/7')
+const del = (id) => {
+	axios.delete(`contribuintes/${id}`)
 		.then(response => {
 			console.log('contribuinte excluido ')
 		})
@@ -90,19 +102,43 @@ const delet = () => {
 }
 
 /*execulta as duas requisicoes e so retorna no 'then' caso as duas requisicaes sejam bem sucedidas*/
-const multiple = () =>{
+const multiple = () => {
 	Promise.all([
-			axios.get('http://localhost:8080/contribuintes'),
-			axios.get('http://localhost:8080/enderecos')
+		axios.get('contribuintes'),
+		axios.get('enderecos')
 	]).then(response => {
-		console.table(response.[0].data)
-		console.table(response.[1].data)
+		console.table(response[0].data)
+		console.table(response[1].data)
 	})
+}
+
+
+function loadTableData(data){
+	const tableBody  = document.getElementById('tableData')
+	let dataHtml = ''
+		
+	for(let person of data){
+		
+		dataHtml += `<tr>
+				<td> <input type="button" onClick={del(${person.id})}  value="Deletar" ></td>
+				<td>${person.nome}</td>
+				<td>${person.documento}</td>
+			</tr>`
+	}
+	
+	tableBody.innerHTML = dataHtml
+}
+
+const handlerDeletar = (id) =>
+{
+   
+	alert('item removido ' + id)
 }
 
 document.getElementById('pesquisar').addEventListener('click',get)
 document.getElementById('cadastrar').addEventListener('click',post)
 document.getElementById('atualizar').addEventListener('click',put)
-document.getElementById('deletar').addEventListener('click',delet)
+document.getElementById('deletar').addEventListener('click',del)
+
 
 
