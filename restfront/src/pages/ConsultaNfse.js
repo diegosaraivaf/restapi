8a91a40f657078e902acdff2ae272d00c403972a
Diego <diego.ferreira@aspec.com.br>
2023-11-17 17:02:46 -0300

    adicionado paginacao

--- restfront/src/pages/ConsultaNfse.js
+++ restfront/src/pages/ConsultaNfse.js
@@ -1,21 +1,29 @@
 
-import { Button, Container, Grid, Paper, Stack, Table, TableBody, TableCell, TableHead, TableRow, TextField } from "@mui/material";
-import { Input } from "../componente/Input";
-import { Head } from "./head";
+import { Button, Container, Grid, Pagination, Paper, Stack, Table, TableBody, TableCell, TableHead, TableRow, TextField } from "@mui/material";
 import { useForm } from "react-hook-form";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import Api from "../componente/Api";
 import {createSearchParams, useNavigate } from "react-router-dom";
+import { SnackbarContext } from "../componente/SnackbarContext";
 
 export function ConsultaNfse() {
-  const { register,  handleSubmit} = useForm();
+  //const { register,  handleSubmit} = useForm();
   const [notas, setNotas] = useState([]);
   const navigate = useNavigate();
+  const {message} = useContext(SnackbarContext)
+  let [page, setPage] = useState(1);
+  let [count, setCount] = useState(1);
   
-  const onSubmit = data => {
+  const pesquisar = data => {
     console.log(data);
-    Api.get('/nfses').then(response =>{
+    Api.get('/nfses',{
+      params: {
+        size: 10
+      }
+    }).then(response =>{
       setNotas(response.data.content)
+      setCount(response.data.totalPages)
+
     }).catch(error =>{
       console.log(error)
     })
@@ -38,38 +46,63 @@ export function ConsultaNfse() {
     });
   }
 
+  const excluir = (id)=>{
+    Api.delete(`/nfses/${id}`).then(response =>{
+      message('Nfse excluida com sucesso ')
+    }).catch(error =>{
+      console.log(error)
+      message(error,'error')
+    })
+  }
+
+  const mudarPagina =(e, p)=>{
+    setPage(p)
+    console.log(p)
+
+    Api.get('/nfses',{
+      params: {
+        page: p-1,
+        size: 10
+      }
+    }).then(response =>{
+      setNotas(response.data.content)
+      setCount(response.data.totalPages)
+    }).catch(error =>{
+      console.log(error)
+    })
+  }
+
   return (
     <>
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form>
     <Paper className="container" >
       <Grid container spacing={2}>
         <Grid item xs={12} sm={4}>
-          <TextField label="Id" {...register("id")} fullWidth/>
+          <TextField label="Id" fullWidth/>
         </Grid>
         <Grid item xs={12} sm={4}>
-          <TextField label="Tipo" {...register("tipo")} fullWidth/>
+          <TextField label="Tipo"  fullWidth/>
         </Grid>
         <Grid item xs={12} sm={4}>
-          <TextField label="Valor" {...register("valor")} fullWidth/>
+          <TextField label="Valor" fullWidth/>
         </Grid>
         <Grid item xs={12} sm={6}>
-          <TextField label="Documento" {...register("documento")} fullWidth/>
+          <TextField label="Documento" fullWidth/>
         </Grid>
         <Grid item xs={12} sm={6}>
-          <TextField label="Nome" {...register("nome")}fullWidth/>
+          <TextField label="Nome" fullWidth/>
         </Grid>
         <Grid item xs={12} sm={4}>
-          <TextField label="Situacao" {...register("situacao")} fullWidth/>
+          <TextField label="Situacao" fullWidth/>
         </Grid>
         <Grid item xs={12} sm={4}>
-          <TextField label="Data Emissao" {...register("dataEmissao")} fullWidth/>
+          <TextField label="Data Emissao" fullWidth/>
         </Grid>
       </Grid>
 
-
       <Stack direction={"row"} justifyContent="space-between" sx={{ mt: 2 }}>
-          <Button onClick={e=> {window.location.href = `/CadastroNfse`}} variant="contained" color="secondary" >Cadastro</Button>
-          <Button type="submit" variant="contained"  >Pesquisar</Button>
+          <Button onClick={e=> navigate("/CadastroNfse")} variant="contained" color="secondary" >Cadastro</Button>
+          <Button onClick={pesquisar} variant="contained"  >Pesquisar</Button>
      </Stack>
     </Paper>
 
@@ -98,11 +131,15 @@ export function ConsultaNfse() {
               <TableCell>{row.tomador.nome}</TableCell>
               <TableCell>{row.valorServico}</TableCell>
               <TableCell>{row.situacaoNfse}</TableCell>
-              <TableCell><Button onClick={e =>{ editar(row.id)}}>Editar</Button> </TableCell>
+              <TableCell>
+                <Button onClick={e =>{ editar(row.id)}}>Editar</Button> 
+                <Button onClick={e =>{ excluir(row.id)}}>Excluir</Button> 
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
       </Table>
+      <Pagination  count={count} page={page} onChange={mudarPagina} color="primary"/>
     </Paper>
     </form>
     </>
