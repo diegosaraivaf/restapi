9e6cdae89c04059802143eaaebb762b21b31fdd6
Diego <diego.ferreira@aspec.com.br>
2023-12-22 17:04:38 -0300

    .

--- restfront/src/pages/Home.js
+++ restfront/src/pages/Home.js
@@ -2,25 +2,30 @@ import { Button, Snackbar } from "@mui/material";
 import { Head } from "./head";
 import { useContext, useState } from "react";
 import { SnackbarContext } from "../componente/SnackbarContext";
+import Api from "../componente/Api";
 
 export function Home({titulo,descricao}) {
   const {message} = useContext(SnackbarContext)
  
 
-  const enviar =()=>{
-    message('teste','warning')
+  const testar =()=>{
+    Api.patch("lancamentos/teste/1",{
+      valor:'100',
+      dataEmissao:'2023-12-22',
+      // situacaoLancamento:'EMITIDO',
+      // parcelas:[{id:'1',valor:'100',situacao:'EMITIDA',dataVencimento:'2023-12-22'}],
+      contribuinte:{id:'1',nome:'teste',documento:'teste'}
+
+    })
 
   }
 
-  // const mostrarMensagem =(mensagem)=>{
-  //   setMessage({open:true, message:'teste'})
-  // }
 
 
 
   return (
     <>
-      <Button onClick={enviar} >show </Button>
+      <Button variant="contained" onClick={testar} >Testar Path</Button>
 
 
     </>
